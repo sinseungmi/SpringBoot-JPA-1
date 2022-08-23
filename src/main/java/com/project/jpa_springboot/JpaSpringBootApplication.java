ac5c187e3665a91bf62da09cfdf92a384ae7c7e7
sinseungmi <tlstmdal3@gmail.com>
2022-08-23 17:39:08 +0900

    spring boot 프로젝트 생성

---
+++ src/main/java/com/project/jpa_springboot/JpaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.project.jpa_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSpringBootApplication.class, args);
+	}
+
+}
